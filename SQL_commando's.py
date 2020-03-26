e750c50982a46937b79aca97880374ef20a3dfbe
y0urselff <52598379+y0urselff@users.noreply.github.com>
2020-03-26 12:38:02 +0100

    ''

--- SQL_commando's.py
+++ SQL_commando's.py
@@ -5,18 +5,16 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 postgresConnection = psycopg2.connect(user = "postgres",
                                   password = "groep5",
                                   host = "127.0.0.1",
-                                  port = "5432",
-                                  database = "voordeelshop")
+                                  port = "5432",)
+                                  # database = "voordeelshop")
 postgresConnection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
 cursor = postgresConnection.cursor();
 
 '''' creeeren van database'''
 def maak_database():
-    cursor = con.cursor();
     name_database = 'Voordeelshop';
     sqlCreateDatabase = "create database "+name_database+";"
     cursor.execute(sqlCreateDatabase)
-
 
 def maak_tabel():
     name_table = 'Products'
@@ -24,4 +22,5 @@ def maak_tabel():
     cursor.execute(sqlCreateTable)
     postgresConnection.commit()
 
-maak_tabel()
+# maak_tabel()
+maak_database()
