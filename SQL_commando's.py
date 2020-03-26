99347ef30253402a625d6c1bb849d8667e25b2b4
y0urselff <52598379+y0urselff@users.noreply.github.com>
2020-03-26 12:05:16 +0100

    ''

--- SQL_commando's.py
+++ SQL_commando's.py
@@ -2,10 +2,26 @@ import psycopg2
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 '''Verbinden van pymongo en psycopg2 met python'''
-con = psycopg2.connect("user=postgres password='groep5'");
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
+postgresConnection = psycopg2.connect(user = "postgres",
+                                  password = "groep5",
+                                  host = "127.0.0.1",
+                                  port = "5432",
+                                  database = "voordeelshop")
+postgresConnection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
+cursor = postgresConnection.cursor();
 
-cursor = con.cursor();
-name_database = 'Voordeelshop';
-sqlCreateDatabase = "create database "+name_database+";"
-cursor.execute(sqlCreateDatabase)
+'''' creeeren van database'''
+def maak_database():
+    cursor = con.cursor();
+    name_database = 'Voordeelshop';
+    sqlCreateDatabase = "create database "+name_database+";"
+    cursor.execute(sqlCreateDatabase)
+
+
+def maak_tabel():
+    name_table = 'Products'
+    sqlCreateTable = "create table " +name_table+ " ();"
+    cursor.execute(sqlCreateTable)
+    postgresConnection.commit()
+
+maak_tabel
