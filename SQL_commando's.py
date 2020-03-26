9cb61da05944b820d210f4df68ce216782d00052
y0urselff <52598379+y0urselff@users.noreply.github.com>
2020-03-26 13:29:29 +0100

    ''

--- SQL_commando's.py
+++ SQL_commando's.py
@@ -17,10 +17,14 @@ def maak_database():
     cursor.execute(sqlCreateDatabase)
 
 def maak_tabel():
-    name_table = 'Products'
-    sqlCreateTable = "create table " +name_table+ " ();"
-    cursor.execute(sqlCreateTable)
+    tabellen = ["products", "brand", "category", "Sub_category", "Sub_sub_category", "sessions", "orders", "profiles"]
+    for i in tabellen:
+        dropTabel = "DROP TABLE IF EXISTS {}".format(i)
+        createTabel = "CREATE TABLE {}".format(i)
+        cursor.execute(dropTabel)
+        cursor.execute(createTabel)
     postgresConnection.commit()
+    cursor.close()
 
 # maak_tabel()
 maak_database()
