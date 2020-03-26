9fe44ee71cc5349e5ec9ffd81522f7bf5e8a769a
jariwezerhu <jari.wezer@student.hu.nl>
2020-03-26 14:03:14 +0100

    Code nog niet werkend vanwege error bij dropTabel/createTabel.

--- SQL_commando's.py
+++ SQL_commando's.py
@@ -3,24 +3,31 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 '''Verbinden van pymongo en psycopg2 met python'''
 postgresConnection = psycopg2.connect(user = "postgres",
-                                  password = "groep5",
+                                  password = "ovk44yp6",
                                   host = "127.0.0.1",
                                   port = "5432",)
                                   # database = "voordeelshop")
 postgresConnection.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT);
 cursor = postgresConnection.cursor();
 
-'''' creeeren van database'''
+''' creeeren van database'''
 def maak_database():
     name_database = 'Voordeelshop';
-    sqlCreateDatabase = "create database "+name_database+";"
+    sqlDropDatabase = "DROP DATABASE IF EXISTS "+name_database+";"
+    sqlCreateDatabase = "CREATE DATABASE "+name_database+";"
+    cursor.execute(sqlDropDatabase)
     cursor.execute(sqlCreateDatabase)
+    cursor.close
 
 def maak_tabel():
-    name_table = 'Products'
-    sqlCreateTable = "create table " +name_table+ " ();"
-    cursor.execute(sqlCreateTable)
+    tabellen = ["products", "brand", "category", "Sub_category", "Sub_sub_category", "sessions", "orders", "profiles"]
+    for i in tabellen:
+        dropTabel = "DROP TABLE IF EXISTS {}".format(i)
+        createTabel = "CREATE TABLE {}".format(i)
+        cursor.execute(dropTabel)
+        cursor.execute(createTabel)
     postgresConnection.commit()
+    cursor.close()
 
-# maak_tabel()
 maak_database()
+maak_tabel()
